2372f84ce1e55fad84cc7a9f9410d7a523166b6c
KremlevLeo <i@lkremlev.ru>
2023-08-30 20:54:56 +0500

    First task Done

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт максимальное из этих чисел./
+
+//Обявление и чтение переменных
+Console.Write("\nEnter first number: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine());
+int max = 0;
+
+//Сравнение переменных
+if (a > b) max = a;
+else max = b;
+
+//Если переменные равны
+if (a == b) Console.WriteLine($"\na = {a}; b = {b} -> the numbers are equal");
+else Console.Write($"\na = {a}; b = {b} -> max = {max}");
